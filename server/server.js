1933cf3ffed7270f9e89070fef2f56d263ce66c7
joeneedum <joeneedum@gmail.com>
2022-02-21 20:37:19 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json());
 
 app.post('/login', (req,res) => {
     const code = req.body.code
     const spotifyWebApi = new SpotifyWebApi({
-        redirectUri: 'http/localhost:3000',
+        redirectUri: 'http://localhost:3000',
         clientId: '5b424dc8c2424b40bde1bdcaef91e45b',
         clientSecret: '6bc2c03d92e54345a940d0ac5fc844f9',
     })
-    spotifyApi.authorizationCodeGrant(code)
+    spotifyWebApi.authorizationCodeGrant(code)
     .then(data => {
         res.json({
             accessToken: data.body.access_token,
@@ -18,7 +22,10 @@ app.post('/login', (req,res) => {
             expiresIn: data.body.expires_in,
         })
     })
-    .catch(() => {
+    .catch(err => {
+        console.log(err)
         res.sendStatus(400)
     })
 })
+
+app.listen(3001) // running server on port 3001
